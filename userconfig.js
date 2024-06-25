0f9118baee9a8a87572fc2d4b1aaab4bde6bb04a
LazyDevUserX <163187136+LazyDevUserX@users.noreply.github.com>
2024-06-25 16:38:31 +0600

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -3,7 +3,7 @@ let saved_config = JSON.parse(localStorage.getItem("CONFIG"));
 const default_config = {
   overrideStorage: true,
   temperature: {
-    location: "London",
+    location: "Dhaka",
     scale: "C",
   },
   clock: {
@@ -15,7 +15,7 @@ const default_config = {
   openLastVisitedTab: true,
   tabs: [
     {
-      name: "myself",
+      name: "rohan",
       background_url: "src/img/banners/cbg-9.gif",
       categories: [
         {
